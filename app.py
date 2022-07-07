05fa5891355e3f95f8d7776116f357d9e7f10e97
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2022-07-07 20:35:04 +0300

    fix imports

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 
 from aiogram.utils.executor import start_webhook
 
-from botapp.handlers import dp
-from botapp.settings import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
-from botapp.bot_actions import on_startup, on_shutdown
+from handlers import dp
+from settings import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from bot_actions import on_startup, on_shutdown
 
 
 if __name__ == '__main__':
