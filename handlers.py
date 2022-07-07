89a50cbf98f94cbb0d490a17fd25890e56bd9b03
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2022-07-07 20:53:47 +0300

    fix Lera feature

--- handlers.py
+++ handlers.py
@@ -7,7 +7,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    if 'лучше всех' in message.text:
+    if 'лучше всех' in message.text.lower():
         await message.answer('Лерка лучше всех!')
     else:
         await message.answer(f"""Все говорят '''{message.text}''', а ты купи слона""")
