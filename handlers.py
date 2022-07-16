9c1060c40b4d63da08f2176019fb7f2f0293a5b1
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2022-07-16 18:01:19 +0300

    Add buttons

--- handlers.py
+++ handlers.py
@@ -2,9 +2,21 @@ from aiogram import types
 from aiogram.dispatcher import Dispatcher
 
 from settings import bot
+from keyboards import start_keyboard
 
 dp = Dispatcher(bot)
 
+
+@dp.message_handler(commands=['start', 'help'])
+async def start_message(message: types.Message):
+    await message.answer("Привет! Смотри что я умею :)", reply_markup=start_keyboard)
+
+
 @dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+async def terminate_text_message(message: types.Message):
+    if message.text == "Загрузить картинку":
+        await bot.send_message("Ну загружай, кто тебе мешает, ААА??!?!?")
+    elif message.text == "Глянуть что уже есть в базе":
+        await bot.send_message("Ничо нет")
+    else:
+        await message.answer("Нииичо не понимаю, ткни лучше в кнопки", reply_markup=start_keyboard)
---
+++ keyboards.py
@@ -0,0 +1,9 @@
+from ctypes import resize
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+
+image_button = KeyboardButton("Загрузить картинку")
+lookup_button = KeyboardButton("Глянуть что уже есть в базе")
+
+start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(image_button).row(lookup_button)
+
+
