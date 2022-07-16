3c9c7df2ff6aadd72337531c1aeb98a10f6554bd
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2022-07-16 17:02:54 +0000

    fix handlers

--- handlers.py
+++ handlers.py
@@ -9,14 +9,14 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def start_message(message: types.Message):
-    await message.answer("Привет! Смотри что я умею :)", reply_markup=start_keyboard)
+    await message.answer(text="Привет! Смотри что я умею :)", reply_markup=start_keyboard)
 
 
 @dp.message_handler()
 async def terminate_text_message(message: types.Message):
     if message.text == "Загрузить картинку":
-        await bot.send_message("Ну загружай, кто тебе мешает, ААА??!?!?")
+        await message.answer(text="Ну загружай, кто тебе мешает, ААА??!?!?")
     elif message.text == "Глянуть что уже есть в базе":
-        await bot.send_message("Ничо нет")
+        await message.answer(text="Ничо нет")
     else:
-        await message.answer("Нииичо не понимаю, ткни лучше в кнопки", reply_markup=start_keyboard)
+        await message.answer(text="Нииичо не понимаю, ткни лучше в кнопки", reply_markup=start_keyboard)
