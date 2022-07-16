64c03c7f97aa784a33e9f4039aaca1051afb58ad
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2022-07-16 18:03:07 +0000

    add images uploader

--- handlers.py
+++ handlers.py
@@ -1,8 +1,12 @@
+import logging
+
 from aiogram import types
 from aiogram.dispatcher import Dispatcher
 
 from settings import bot
 from keyboards import start_keyboard
+
+logger = logging.getLogger(__name__)
 
 dp = Dispatcher(bot)
 
@@ -12,8 +16,20 @@ async def start_message(message: types.Message):
     await message.answer(text="Привет! Смотри что я умею :)", reply_markup=start_keyboard)
 
 
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def get_photo(message: types.Message):
+    photo_id = message.photo[-1].file_id
+    photo = await bot.get_file(photo_id)
+    photo_path = photo.file_path
+    await bot.download_file(photo_path, "last_photo.jpg")
+    await message.answer(photo_id)
+    await message.answer(photo)
+    await message.answer(photo_path)
+
+
 @dp.message_handler()
 async def terminate_text_message(message: types.Message):
+    print(message)
     if message.text == "Загрузить картинку":
         await message.answer(text="Ну загружай, кто тебе мешает, ААА??!?!?")
     elif message.text == "Глянуть что уже есть в базе":
